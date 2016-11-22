d1a8a2a0d2ba14211cb28db38d90693c6a95241e
Lokman <loklek-5@student.ltu.se>
2016-11-23 00:06:21 +0100

    ajout de carte

--- Back-end/Public/js/myjs.js
+++ Back-end/Public/js/myjs.js
@@ -2,8 +2,14 @@
   'use strict'
   var app = angular.module('Application',['ngMaterial','ngRoute','ngMessages','material.svgAssetsCache']);
 
-
+  app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+});
   app.controller('FirstController', ['$http', '$scope', function($http,$scope){
+   
       $scope.resFacebook = [];
       $scope.resGoogle = [];
       $scope.resAutres = [];
